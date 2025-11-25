03d33c7058d02b89a64d1b8e3c6fac8f745e1061
Samaras7 <93186511+Samaras7@users.noreply.github.com>
2025-11-25 18:31:35 +0100

    Improve pie form validation and cart removal

--- Bakery/Models/IPieRepository.cs
+++ Bakery/Models/IPieRepository.cs
@@ -5,5 +5,8 @@
         IEnumerable<Pie> AllPies { get; }
         IEnumerable<Pie> PiesOfTheWeek { get; }
         Pie? GetPieById(int pieId);
+        Pie AddPie(Pie pie);
+        bool UpdatePie(Pie pie);
+        bool DeletePie(int pieId);
     }
 }
